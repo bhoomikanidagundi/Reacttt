14812655d4b12569802a8c072769d8e6826f4a63
bhoomikanidagundi <bhoomikanidagundi063@gmail.com>
2025-03-29 15:52:48 +0530

    Delete Main.jsx

--- Main.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-export default function counter(){
-    const [count,setCount]=useState(0);
-    return (
-        <div>
-            <button>count:{count}</button>
-        </div>
-    )
-}
